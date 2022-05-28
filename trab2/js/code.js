37a7b29b7702b0b81248a128e735607ef9dc7df7
Xenos <artemmordan@gmail.com>
2022-05-28 16:21:56 +0100

    Added: Rocket Object

--- trab2/js/code.js
+++ trab2/js/code.js
@@ -9,6 +9,8 @@ const step = 300;
 const R = 20; // Planet Radius
 const NUMBER_OF_SPACE_TRASH = 20;
 
+var space_ship;
+
 function sphericalToCartesian(lat, long, r) {
     return new THREE.Vector3(
         r * Math.cos(long) * Math.sin(lat),
@@ -36,6 +38,7 @@ function createScene() {
 function animate() {
     update();
     display();
+    //space_ship.rotateY(THREE.Math.degToRad(2));
     requestAnimationFrame(animate);
 }
 
@@ -64,11 +67,13 @@ function init() {
     createCameras();
 
     // TODO create planet, rocket and junk
+    /*
     createPlanet();
     for(var i = 0; i < NUMBER_OF_SPACE_TRASH; i++) {
         addSpaceTrash();
     }
-
+    */
+    createSpaceShipObject();
     clock = new THREE.Clock();
 }
 
@@ -242,6 +247,120 @@ function onKeyUp(e) {
 
 //ESTOU AINDA A TESTAR
 
+/*
+*   nose
+*   section_1
+*   section_2 () (window)
+*   main_body \\ (leg)
+*   base       \\
+*   nozzle
+*             capsule
+*
+*
+* */
+
+function createSpaceShipObject() {
+    const size_metric = R/5;
+
+    //MAIN BODY
+    const main_body_g = new THREE.CylinderGeometry( size_metric , size_metric, size_metric, 32 );
+    const material = new THREE.MeshBasicMaterial( {color: 0xffff00, wireframe: true} );
+    const material2 = new THREE.MeshBasicMaterial( {color: 0x00ff00, wireframe: true} );
+    const material3 = new THREE.MeshBasicMaterial( {color: 0x1e81b0, wireframe: true} );
+    const main_body = new THREE.Mesh( main_body_g, material );
+
+    //SECTION_2
+    const section_2_g = new THREE.CylinderGeometry( size_metric/1.2 , size_metric, size_metric, 32 );
+    const section_2 = new THREE.Mesh(section_2_g, material);
+    section_2.position.y = size_metric;
+    main_body.add(section_2);
+
+
+    //WINDOW OUTER
+    const window_o_main_object = new THREE.Object3D();
+    const window_o_g = new THREE.CylinderGeometry( size_metric/3 , size_metric/3, size_metric/ 8, 32 );
+    const window_o = new THREE.Mesh(window_o_g, material2);
+    window_o.position.y = 0.9 * size_metric;
+    window_o_main_object.add(window_o)
+
+    window_o_main_object.rotateX(THREE.Math.degToRad(90));
+    window_o_main_object.rotateY(THREE.Math.degToRad(-10));
+    window_o_main_object.rotateZ(THREE.Math.degToRad(90));
+
+    const window_inner_o = new THREE.CapsuleGeometry( size_metric/4, 0.5, 4, 8 );
+    const window_inner = new THREE.Mesh(window_inner_o, material3);
+    window_o.add(window_inner);
+    section_2.add(window_o_main_object);
+
+    //SECTION_1
+    const section_1_g = new THREE.CylinderGeometry( size_metric/1.8 , size_metric/1.2, size_metric, 32 );
+    const section_1 = new THREE.Mesh(section_1_g, material);
+    section_1.position.y = 2 * size_metric;
+    main_body.add(section_1);
+
+    //NOSE
+    const nose_g = new THREE.CylinderGeometry( 0 , size_metric/1.8, size_metric, 32 );
+    const nose = new THREE.Mesh(nose_g, material);
+    nose.position.y = 3 * size_metric;
+    main_body.add(nose);
+
+    //BASE
+    const base_g = new THREE.CylinderGeometry( size_metric , size_metric/1.2, size_metric, 32 );
+    const base = new THREE.Mesh(base_g, material);
+    base.position.y = -1 * size_metric;
+    main_body.add(base);
+
+    //NOZZLE
+    const nozzle_g = new THREE.CylinderGeometry( size_metric/1.5 , size_metric/1.5, size_metric/2, 32 );
+    const nozzle = new THREE.Mesh(nozzle_g, material);
+    nozzle.position.y = -1.7 * size_metric;
+    main_body.add(nozzle);
+
+    //LEGS
+    const leg_object_1 = createLegObject(size_metric);
+    const leg_object_2 = createLegObject(size_metric);
+    leg_object_2.rotateY(THREE.Math.degToRad(90));
+    const leg_object_3 = createLegObject(size_metric);
+    leg_object_3.rotateY(THREE.Math.degToRad(180));
+    const leg_object_4 = createLegObject(size_metric);
+    leg_object_4.rotateY(THREE.Math.degToRad(270));
+
+
+    main_body.add(leg_object_1);
+    main_body.add(leg_object_2);
+    main_body.add(leg_object_3);
+    main_body.add(leg_object_4);
+
+    space_ship = main_body;
+    space_ship.rotateY(THREE.Math.degToRad(0))
+    scene.add(main_body);
+}
+
+
+function createLegObject(size_metric) {
+    const material = new THREE.MeshBasicMaterial( {color: 0xffff00, wireframe: true} );
+
+    const leg_main_object = new THREE.Object3D();
+    const leg_object = new THREE.Object3D();
+    const leg_g = new THREE.CylinderGeometry( size_metric/8 , size_metric/8, size_metric * 2, 32 )
+    const leg = new THREE.Mesh(leg_g, material);
+
+    leg_object.add(leg);
+    leg_object.position.x = size_metric;
+    leg_object.rotateZ(THREE.Math.degToRad(30));
+    leg.position.y= -0.7 * size_metric;
+
+
+    //CAPSULE
+    const capsule_g = new THREE.CapsuleGeometry( size_metric/4, 2 * size_metric, 4, 8 );
+    const capsule = new THREE.Mesh(capsule_g, material);
+    capsule.position.y= -size_metric;
+    capsule.rotateZ(THREE.Math.degToRad(-30));
+    leg.add(capsule);
+
+    leg_main_object.add(leg_object);
+    return leg_main_object;
+}
 
 
 //TODO (ARTEM) CRIAR CLASSE DA SPACE_SHIP
